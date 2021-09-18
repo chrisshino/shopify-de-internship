6ef28c6a04bf5673b9f135e6fe5b29b89865c60c
chris shino <chris.shino@hotmail.com>
2021-09-17 22:31:43 -0400

    got some end points working :)

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,66 @@
-from flask import Flask 
+from flask import Flask, request 
+from flask_restx import Api, Resource, fields
+from config import DevConfig
+from models import Recipe
+from ext import db
 
 app = Flask(__name__)
+app.config.from_object(DevConfig)
+db.init_app(app)
+api=Api(app, doc='/docs')
 
-@app.route('/')
-def index():
-  return 'hello world'
 
-@app.route('/<hello>')
-def hello(hello):
-  if hello == "me":
-    return 'yo'
-  else:
-    return "wooppps"
+#model (serializer into JSON)
+recipe_model=api.model(
+  "Recipe",
+  {
+    "id":fields.Integer(),
+    "title":fields.String(),
+    "description":fields.String()
+  }
+)
 
-    
+@api.route('/recipes')
+class RecipesResource(Resource):
+  @api.marshal_list_with(recipe_model)
+  def get(self):
+   """Get's all images"""
+   recipes=Recipe.query.all()
+
+   return recipes
+
+  @api.marshal_with(recipe_model)
+  def post(self):
+    data=request.get_json()
+
+    new_recipe=Recipe(
+      title=data.get('title'),
+      description=data.get('description'),
+    )
+
+    new_recipe.save()
+    return new_recipe, 201
+   
+
+@api.route('/recipe/<int:id>')
+class RecipeResource(Resource):
+  @api.marshal_with(recipe_model)
+  def get(self, id):
+    recipe=Recipe.query.get_or_404(id)
+    return recipe
+
+  @api.marshal_with(recipe_model)
+  def delete(self, id):
+    recipe_to_delete=Recipe.query.get_or_404(id)
+    recipe_to_delete.delete()
+    return recipe_to_delete
+
+@app.shell_context_processor
+def make_shell_context():
+  return {
+    "db":db,
+    "Recipe":Recipe
+  }
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True,port=80)
+    app.run()
